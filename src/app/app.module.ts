7052a0fc4ab13b8e52683843fa7ca98bfc780710
Amalpm7 <amal.p@nestgroup.net>
2022-10-09 17:29:04 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddemployComponent } from './addemploy/addemploy.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddemployComponent
+    AddemployComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
