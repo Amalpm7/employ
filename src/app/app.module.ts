f42f1fbba2cfdedf202d72ce8abbaec711248e47
Amalpm7 <amal.p@nestgroup.net>
2022-10-09 17:08:02 +0530

    addemp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddemployComponent } from './addemploy/addemploy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddemployComponent
   ],
   imports: [
     BrowserModule,
